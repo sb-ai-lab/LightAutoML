08a72ff2ce82dd987130658b84c2cc697f84ace8
Alexander Kirilin <adkirilin@gmail.com>
2022-08-04 15:39:32 +0300

    Update script (#13)

--- scripts/poetry_fix.py
+++ scripts/poetry_fix.py
@@ -10,16 +10,26 @@ from typing import Optional
 
 
 PYPROJECT_TOML = Path("pyproject.toml")
-DEFAULT_PYTHON_DEPS = ">=3.6.1, <3.10"
+ALL_PYTHON_DEPS = ">=3.6.1, <3.10"
 PYTHON_DEPS = {6: "~3.6.1", 7: "~3.7.1", 8: "~3.8.0", 9: "~3.9.0"}
 PYTHON_DEPS_PATTERN = '^python = ".*"$'
+
+
+def _check_py_version():
+    py_version = sys.version_info
+
+    if py_version.major != 3:
+        raise RuntimeError("Works only with python 3")
+
+    if py_version.minor not in PYTHON_DEPS:
+        raise RuntimeError(f"Works only with python 3.[{list(PYTHON_DEPS)}]")
 
 
 def _set_version(py_version: Optional[int] = None):
     for line in fileinput.input(PYPROJECT_TOML.name, inplace=1):
         if re.search(PYTHON_DEPS_PATTERN, line):
             if py_version is None:
-                version = DEFAULT_PYTHON_DEPS
+                version = ALL_PYTHON_DEPS
             else:
                 version = PYTHON_DEPS[py_version]
             line = 'python = "{}"\n'.format(version)
@@ -29,20 +39,24 @@ def _set_version(py_version: Optional[int] = None):
 
 def main():
     """Cli."""
+    _check_py_version()
+
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-v",
-        "--version",
-        nargs="?",
-        type=int,
-        choices=list(PYTHON_DEPS.keys()),
-        required=False,
-        default=None,
-        help="Set python restriction in `pyproject.toml`",
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-c",
+        "--current",
+        action="store_true",
+        help="Set curret python version in `pyproject.toml`",
     )
+    group.add_argument("-f", "--full", action="store_true", help="Set all pythons versions in `pyproject.toml`")
 
     args = parser.parse_args()
-    _set_version(args.version)
+
+    if args.current:
+        _set_version(sys.version_info.minor)
+    elif args.full:
+        _set_version(None)
 
 
 if __name__ == "__main__":
