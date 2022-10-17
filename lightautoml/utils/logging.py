9747c9ea6d47e7538dfba4ec5e210e313937ec5c
Boris <bormix@inbox.ru>
2022-10-17 20:01:16 +0300

    Update logging.py

--- lightautoml/utils/logging.py
+++ lightautoml/utils/logging.py
@@ -175,7 +175,6 @@ def add_filehandler(filename: str, level=logging.DEBUG):
             if type(handler) == logging.FileHandler:
                 _logger.handlers.remove(handler)
 
-
 class DuplicateFilter(object):
     def __init__(self):
         self.msgs = set()
