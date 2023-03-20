2a5805515040d8c7406d530eacd35bb2215198a0
dev-rinchin <rinchin.damdinov@yandex.ru>
2023-03-20 14:25:59 +0000

    fix precommit

--- tests/unit/test_transformers/test_numeric.py
+++ tests/unit/test_transformers/test_numeric.py
@@ -1,12 +1,12 @@
-
 from lightautoml.transformers.numeric import FillnaMean
 from lightautoml.transformers.numeric import FillnaMedian
+from lightautoml.transformers.numeric import QuantileBinning
 
 
 def test_fillnamean(lamldataset_with_na):
     transformer = FillnaMean()
     output = transformer.fit_transform(lamldataset_with_na)
-    
+
     assert output.data[:, 0].mean() == 4
     assert output.data[:, 1].mean() == 5
     assert output.data[:, 2].mean() == 0
@@ -15,7 +15,7 @@ def test_fillnamean(lamldataset_with_na):
 def test_fillnamedian(lamldataset_with_na):
     transformer = FillnaMedian()
     output = transformer.fit_transform(lamldataset_with_na)
-    
+
     assert output.data[:, 0].mean() == 4
     assert output.data[:, 1].mean() == 5
     assert output.data[:, 2].mean() == 0
