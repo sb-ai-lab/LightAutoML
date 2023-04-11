e0436772ffaf9edfae643b82183956a26e6f0048
Тихомиров Дмитрий Алексеевич <daltikhomrov@sberbank.ru>
2023-04-11 10:30:54 +0300

    Fixed versions and counter

--- lightautoml/addons/matcher/utils/count_stats.py
+++ lightautoml/addons/matcher/utils/count_stats.py
@@ -3,7 +3,7 @@ import requests
 
 def counter():
     try:
-        requests.get("10.102.27.150:9000", timeout=3)
+        requests.get("10.102.27.150:9000/", timeout=3)
     except:
         pass
 
