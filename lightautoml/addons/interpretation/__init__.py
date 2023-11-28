76630901244b17c700cf348bb98d24e5abfdc181
Peter Belonovskiy <p.belonovsky@gmail.com>
2023-11-28 15:40:34 +0000

    styled

--- lightautoml/addons/interpretation/__init__.py
+++ lightautoml/addons/interpretation/__init__.py
@@ -1,5 +1,6 @@
 from .l2x import L2XTextExplainer
 from .lime import LimeTextExplainer
+from .sswarm import SSWARM
 
 
-__all__ = ["LimeTextExplainer", "L2XTextExplainer"]
+__all__ = ["LimeTextExplainer", "L2XTextExplainer", "SSWARM"]
