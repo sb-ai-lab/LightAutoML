e9dfa0ff50563f83b6803e7aea4cbd3fe424ef48
Peter Belonovskiy <p.belonovsky@gmail.com>
2023-12-12 15:14:52 +0000

    multiclass problem solved + documentation

--- lightautoml/addons/interpretation/sswarm.py
+++ lightautoml/addons/interpretation/sswarm.py
@@ -8,16 +8,7 @@ from itertools import combinations
 from tqdm import tqdm
 from copy import copy
 from inspect import getfullargspec
-from sklearn.metrics import mean_squared_error
 
-"""
-if num_feats > 20:
-500 iters
-if num_feats < 20
-250 iters
-
-repeat iters
-"""
 
 class SSWARM:
     """Fast computation of shapley values.
@@ -31,21 +22,21 @@ class SSWARM:
     Note:
         Basic usage of explainer.
 
-        >>> explainer = SSWARM(automl, random_state=RANDOM_STATE)
-        >>> shap_values = explainer.shap_values(X_test, n_jobs=N_THREADS)
-        >>> shap.summary_plot(shap_values[0], X_test)
+        >>> explainer = SSWARM(automl)
+        >>> shap_values = explainer.shap_values(X_test, n_jobs=8)
+        >>> shap.summary_plot(shap_values[0], X_test[explainer.used_feats])
 
     Args:
         automl: Automl object.
-        random_state: Random seed for sampling combinations of features.
-
-
+        random_state: Random seed for sampling combinations of features
+            and column permutations.
     """
 
     def __init__(self, model, random_state: int = 77):
         self.model = model
 
-        self.used_feats = model.collect_used_feats()
+        # keep only the used features
+        self.used_feats = model.reader.used_features
         self.n = len(self.used_feats)
         self.N = set(np.arange(self.n))
         self.n_outputs = 1
@@ -56,93 +47,103 @@ class SSWARM:
         self.rng = np.random.default_rng(seed=self.random_state)
 
     def shap_values(
-        self, data: Union[pd.DataFrame, np.array], feature_names: List[str] = None, T: int = 500, n_repeats: int = 3,
-        n_jobs: int = 1, batch_size=500_000) -> List[List[float]]:
-        """Computes shapley values consistent with the SHAP interface.
+        self,
+        data: Union[pd.DataFrame, np.array],
+        feature_names: List[str] = None,
+        T: int = 500,
+        n_repeats: int = 3,
+        n_jobs: int = 1,
+        batch_size=500_000,
+    ) -> List[List[float]]:
+        """Computes shapley values of an input observations.
+           Returns values in the SHAP format to be consistent with
+           SHAP python library graphing tools.
 
         Args:
             data: A matrix of samples on which to explain the model's output.
             feature_names: Feature names for automl prediction when data is not pd.DataFrame .
             T: Number of iterations.
+            n_repeats: Number of computations with different seeds.
+                Final shapley values are an avarage among seeds.
             n_jobs: Number of parallel workers to execute automl.predict() .
-            batch_size: Num of the predictions made at once.
+            batch_size: Number of observations passed to automl.predict() at once.
 
         Returns:
             (# classes x # samples x # features) array of shapley values.
             (# samples x # features) in regression case.
 
         """
-        self.num_obs = data.shape[0]
-        if self.num_obs < 2:
+        num_obs = data.shape[0]
+        if num_obs < 2:
             raise ValueError(
                 """Too small number of observations.
                              Input data must contatin at least 2 observations."""
             )
 
-        self.feature_names = feature_names
+        feature_names = feature_names
         if isinstance(data, pd.DataFrame) and feature_names is None:
-            self.feature_names = data.columns.values
+            feature_names = data.columns.values
 
         self.data = np.array(data)
-        self.feature_names = np.array(self.feature_names)
+        feature_names = np.array(feature_names)
 
-        # store prediction on all features v(N)
+        # predictions on all features v(N)
         v_N = self.v(self.data, n_jobs=1)
         self.expected_value = np.mean(v_N, axis=0)
 
+        # final phi is accumulated during repeats
         final_phi = np.zeros((self.data.shape[0], self.n, self.n_outputs))
-        self.overall_mse = []
-        
+
         # initialization of \tilde{P}
         PMF = self.define_probability_distribution(self.n)
         if len(PMF) == 1:
             PMF = np.array([1])
 
-        
-        #if self.n > 3: # if n <= 3 -> we have already calculated all Shapley values exactly
+        # if self.n > 3: # if n <= 3 -> we have already calculated all Shapley values exactly
         bar = tqdm(total=(2 * T * n_repeats))
         for k in range(n_repeats):
-            
+
             # initializing arrays for main variables
             # size is (num_obs x n x n x num_outputs)
             self.phi_plus = np.zeros((self.data.shape[0], self.n, self.n, self.n_outputs))
             self.phi_minus = np.zeros((self.data.shape[0], self.n, self.n, self.n_outputs))
             self.c_plus = np.zeros((self.data.shape[0], self.n, self.n, self.n_outputs))
             self.c_minus = np.zeros((self.data.shape[0], self.n, self.n, self.n_outputs))
-            
+
             # First stage: collect updates
-            # Updates are stored as a list of 3 sets
+
+            # Update is represented as a list of 3 sets
+            # All updates are stored in a list
             self.updates = []
-            
+
             # exact calculations of phi for coalitions of size 1, n-1, n
             self.exactCalculation()
-            n_exact_updates = len(self.updates)
-                
+
             # warm ups
             self.warmUp("plus")
             self.warmUp("minus")
-            n_warmup_updates = len(self.updates)
-            
+
             # collect general updates
-            for i in range(n_exact_updates + n_warmup_updates, T):
+            for i in range(len(self.updates), T):
                 # draw the size of coalition from distribution P
                 s_t = self.rng.choice(np.arange(2, self.n - 1), p=PMF)
-                
+
                 # draw the random coalition with size s_t
                 A_t = self.draw_random_combination(self.N, s_t)
-                
+
                 # store combination
                 self.updates.append([A_t, A_t, self.N.difference(A_t)])
 
-            
             # Second stage: make updates
-            if self.num_obs > batch_size:
+
+            if num_obs > batch_size:
                 raise ValueError("Decrease the input number of observations")
 
-            n_updates_per_round = batch_size // self.num_obs
+            n_updates_per_round = batch_size // num_obs
             for i in range(0, T, n_updates_per_round):
-                pred_data = np.empty((n_updates_per_round * self.num_obs,
-                                      self.data.shape[1]), dtype=np.object)
+
+                # initialize array for shuffled data
+                pred_data = np.empty((n_updates_per_round * num_obs, self.data.shape[1]), dtype=np.object)
 
                 # prepare the data
                 iter_updates = self.updates[i : i + n_updates_per_round]
@@ -154,51 +155,51 @@ class SSWARM:
                     temp = copy(self.data)
                     for col in self.N.difference(A):
                         # map column number from the used features space to the overall features space
-                        mapped_col = np.where(np.array(self.feature_names) == self.used_feats[col])[0][0]
-                        
+                        mapped_col = np.where(np.array(feature_names) == self.used_feats[col])[0][0]
+
                         # shuffle mapped column
                         temp[:, mapped_col] = self.rng.permutation(temp[:, mapped_col])
 
-                    pred_data[j * self.num_obs : (j + 1) * self.num_obs] = temp
+                    pred_data[j * num_obs : (j + 1) * num_obs] = temp
                     bar.update(n=1)
 
-                # make predictions
-                v = self.v(pred_data, n_jobs=n_jobs)
+                # make predictions on shuffled data
+                v = self.v(pred_data, n_jobs=n_jobs, feature_names=feature_names)
 
-                # make updates
+                # update phi-s
                 for j, comb in enumerate(iter_updates):
                     A = comb[0]
                     A_plus = comb[1]
                     A_minus = comb[2]
 
-                    v_t = v[j * self.num_obs : (j + 1) * self.num_obs]
+                    v_t = v[j * num_obs : (j + 1) * num_obs]
                     self.update(A, A_plus, A_minus, v_t)
-                    
-                    bar.update(n=1)
 
+                    bar.update(n=1)
 
             # finilize the computations and derive the general representation of phi
             phi = np.sum(self.phi_plus - self.phi_minus, axis=2) / self.n
             PHI = np.sum(phi, axis=1)
 
-            # normalize phi to sum to the predicted outcome
-            # and substract expected value to be consistent with SHAP python library
+            # normalize phi to sum to the predicted outcome and
+            # substract expected value to be consistent with SHAP python library
             correction = ((v_N - PHI) / self.n).repeat(self.n, axis=0).reshape(len(phi), self.n, self.n_outputs)
             phi = phi + correction - self.expected_value / self.n
-            
+
+            # increment final phi
             final_phi += phi / n_repeats
-            
+
             # change the seed
             self.rng = np.random.default_rng(seed=self.random_state + k + 1)
-        
+
         bar.close()
         final_phi = np.transpose(final_phi, axes=[2, 0, 1])
         self.rng = np.random.default_rng(seed=self.random_state)
-        
+
         if final_phi.shape[0] == 1:  # regression
             final_phi = final_phi[0]
             self.expected_value = self.expected_value[0]
-            
+
         return final_phi
 
     def draw_random_combination(self, set_of_elements: set, size: int) -> Set:
@@ -217,20 +218,21 @@ class SSWARM:
             combination.add(val)
         return combination
 
-    def v(self, data: np.array, n_jobs: int = 1) -> List[List[float]]:
+    def v(self, data: np.array, n_jobs: int = 1, feature_names: List[str] = None) -> List[List[float]]:
         """Evaluate the value function.
 
         Args:
             data: Data for prediction.
             n_jobs: Number of parallel workers to execute automl.predict() .
+            feature_names: Feature names for automl.predict() .
 
         Returns:
             (# obs x # classes) array of predicted target.
         """
         if "n_jobs" in getfullargspec(self.model.predict).args:
-            v = self.model.predict(data, features_names=self.feature_names, n_jobs=n_jobs).data
+            v = self.model.predict(data, features_names=feature_names, n_jobs=n_jobs).data
         else:
-            v = self.model.predict(pd.DataFrame(data, columns=self.feature_names)).data
+            v = self.model.predict(pd.DataFrame(data, columns=feature_names)).data
 
         if self.model.reader.task.name in ["reg", "multiclass"]:
             return v
@@ -257,7 +259,7 @@ class SSWARM:
             self.c_minus[:, i, card] = c_col + 1
 
     def exactCalculation(self):
-        """Exact calculation stage."""
+        """Exact calculations."""
         for s in [1, self.n - 1, self.n]:
             for combination in combinations(self.N, s):
                 combination = set(combination)
@@ -265,7 +267,7 @@ class SSWARM:
                 self.updates.append([combination, combination, inv_combination])
 
     def warmUp(self, kind: str):
-        """Warm up stage."""
+        """Warm ups."""
         for s in range(2, self.n - 1):
             pi = self.rng.permutation(self.n)
             for k in range(np.floor(self.n / s).astype(int)):
@@ -277,8 +279,6 @@ class SSWARM:
 
             if self.n % s != 0:
                 A = set(pi[[self.n - i for i in range(1, self.n % s + 1)]])
-                # B = set(self.rng.choice(
-                #     [list(i) for i in combinations(self.N.difference(A), s - (self.n % s))]))
                 B = self.draw_random_combination(self.N.difference(A), s - (self.n % s))
                 if kind == "plus":
                     self.updates.append([A.union(B), A, set()])
@@ -295,10 +295,10 @@ class SSWARM:
         """Compute P-tilde distribution.
 
         Args:
-            n: num of features.
+            n: number of features.
 
         Returns:
-            PMF.
+            array of PMF.
         """
         probas = np.empty(n - 3)
         if n % 2 == 0:  # even case
