006b02afd800ec3c645b4e5edcefd81007c749c3
Rinchin <rinchin.damdinov@yandex.ru>
2024-12-10 14:02:49 +0000

    update lib version

--- scripts/poetry_fix.py
+++ scripts/poetry_fix.py
@@ -10,7 +10,7 @@ from typing import Optional
 
 
 PYPROJECT_TOML = Path("pyproject.toml")
-ALL_PYTHON_DEPS = ">=3.8, <3.13"
+ALL_PYTHON_DEPS = ">=3.8"
 PYTHON_DEPS = {8: "~3.8.0", 9: "~3.9.0", 10: "~3.10.0", 11: "~3.11.0", 12: "~3.12.0"}
 PYTHON_DEPS_PATTERN = '^python = ".*"$'
 
